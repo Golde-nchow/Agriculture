fe7ec1f502f7e0532106c1d01271acd984cbc783
Golden_Chow <570384231@qq.com>
2019-03-02 18:35:55 +0800

    链表：在单链表和双链表中删除倒数第K个节点（两个题目）

---
+++ 程序员代码面试指南/src/list/在单链表和双链表中删除倒数第K个节点/SingleNode.java
@@ -0,0 +1,16 @@
+package list.在单链表和双链表中删除倒数第K个节点;
+
+/**
+ * TODO 2019/2/27 :
+ *
+ * @author User : Golden_chow
+ */
+@SuppressWarnings("all")
+public class SingleNode {
+    public int value;
+    public SingleNode next;
+
+    public SingleNode(int data) {
+        this.value = data;
+    }
+}
