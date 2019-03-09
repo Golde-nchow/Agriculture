5b2272d2450a704f58637231e06ffd24d63b9f7e
Golden_Chow <570384231@qq.com>
2019-03-09 23:04:31 +0800

    判断单链表是否回文结构，要求额外空间复杂度为O(1)。难度：★★

--- src/main/java/程序员代码面试指南/list/JudgeChainInList/IfChainInList.java
+++ src/main/java/程序员代码面试指南/list/JudgeChainInList/IfChainInList.java
@@ -11,12 +11,15 @@ import java.util.Stack;
 public class IfChainInList {
 
     public static void main(String[] args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(2);
-        head.next.next.next = new Node(1);
-        head.next.next.next.next = new Node(5);
-        System.out.println("回文结构：" + myJudgeChain(head));
+        Node head = new Node(6);
+        head.next = new Node(5);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(2);
+        head.next.next.next.next.next = new Node(1);
+        head.next.next.next.next.next.next = new Node(5);
+        head.next.next.next.next.next.next.next = new Node(6);
+        System.out.println("回文结构：" + getJudgeChainPlus(head));
     }
 
     /**
@@ -48,7 +51,53 @@ public class IfChainInList {
      * @return 是否是回文结构，是则返回 true, 否则返回 false
      */
     public static boolean getJudgeChainPlus(Node head) {
-        return false;
+        Node helpLeft = head;
+        Node left = head;
+        // 取出中间节点
+        while (helpLeft.next != null && helpLeft.next.next != null) {
+            left = left.next;
+            helpLeft = helpLeft.next.next;
+        }
+
+        // 分裂链表
+        Node right = left.next; // 右链表
+        left.next = null; // 左链表
+
+        // 右链表反转
+        Node next = null;
+        Node pre = null;
+        while (right != null) {
+            next = right.next;
+            right.next = pre;
+            pre = right;
+            right = next;
+        }
+        // 保存节点：刚刚反转后，右链表需要重新回到开始处，否则就是null了
+        right = pre;
+        // 左链表回到开头处
+        left = head;
+        // 开始比较
+        boolean result = true;
+        while (left != null && right != null) {
+            if (left.value != right.value) {
+                result = false;
+                break;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        // 比较完成后，右链表回到开始处
+        right = pre.next;
+        //
+        pre.next = null;
+        // 还原链表
+        while (right != null) {
+             left = right.next;
+             right.next = pre;
+             pre = right;
+             right = left;
+        }
+        return result;
     }
 
 }
